c2e6b5e08d49c85b635b9359fa43522980d44a18
Alf Fenton <fentonalf@gmail.com>
2022-06-30 17:55:34 -0500

    implemented canvas rerenderer

--- pxcanvas/pxcanvas.go
+++ pxcanvas/pxcanvas.go
@@ -6,6 +6,7 @@ import (
 	"pixl/apptype"
 
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -58,4 +59,23 @@ func NewPxCanvas(state *apptype.State, config apptype.PxCanvasConfig) *PxCanvas 
 	pxCanvas.PixelData = NewBlankImage(pxCanvas.PxCols, pxCanvas.PxRows, color.NRGBA{128, 128, 128, 255})
 	pxCanvas.ExtendBaseWidget(pxCanvas)
 	return pxCanvas
+}
+
+func (pxCanvas *PxCanvas) CreateRenderer() fyne.WidgetRenderer {
+	canvasImage := canvas.NewImageFromImage(pxCanvas.PixelData)
+	canvasImage.ScaleMode = canvas.ImageScalePixels
+	canvasImage.FillMode = canvas.ImageFillContain
+
+	canvasBorder := make([]canvas.Line, 4)
+	for i := 0; i < len(canvasBorder); i++ {
+		canvasBorder[i].StrokeColor = color.NRGBA{100, 100, 100, 255}
+		canvasBorder[i].StrokeWidth = 2
+	}
+	renderer := &PxCanvasRenderer{
+		pxCanvas:     pxCanvas,
+		canvasImage:  canvasImage,
+		canvasBorder: canvasBorder,
+	}
+	pxCanvas.renderer = renderer
+	return renderer
 }
